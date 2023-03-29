fe0733e5e221d87dbcd01d8639f3fa513b80af5e
ShankarRaagav <127303292+ShankarRaagav@users.noreply.github.com>
2023-03-29 19:20:24 +0530

    Add files via upload

--- automation.js
+++ automation.js
@@ -1,11 +1,12 @@
 const { selectdrop } = require ("../Pages/omayo");
 const omayo = require("../Pages/omayo");
+const { seeNthTodoEquals } = require("../Pages/todoPages");
 
-Feature("Testing login details");
+Feature("Testing scenarios in omayo blogspot");
 
 Before(async ({ I, Omayo }) => 
 {
-	await I.say('I amm on Omayo Blog');
+	await I.say('I am on Omayo Blog');
 	await I.amOnPage("https://omayo.blogspot.com/");
 });
 
@@ -13,24 +14,132 @@ const username = 'input[name="userid"]';
 const email = 'raagav@gmail.com';
 const paswd = 'input[name="pswrd"]';
 const pass = '010996';
-const login = '#form input[type = button]';
+const login = '#form input[type="button"]';
+const content = 'textarea[cols="80"]';
+const text = 'Welcome to Omayo Blogspot. Please share you thoughts with our blog!!!';
+//const blogs = 'span[id="blogsmenu"]';
+//const Button1 = 'button#but1';
+//const Button2 = 'button#but2';
+const svalue = "subscribe";
+const sbox = 'input.gsc-input';
+const Search = 'input.gsc-search-button';
+const Dropdown = 'button.dropbtn';
+const preloadtext = '#textbox1';
 
 
-/**
- * Happy Path tests
- */
 
 
-Scenario.only("Login with username and password", async ({ I, Omayo }) => 
+Scenario("Enter text in dialogue box", async({I, Omayo}) =>
+{
+	await I.say("When I enter text");
+	await omayo.value(content, text);
+	await I.wait(10);
+});
+
+
+
+Scenario("1. Verify selenium by arun page after mouseover and click", async({I, Omayo})=>
+{
+	await I.say('Mouseover on blogsmenu and click on SeleniumbyArun');
+	await omayo.hoverTo();
+	await I.wait(2);
+	await omayo.select();
+	await I.say('SeleniumbyArun is shown in blogs dropdown');
+	await omayo.validate();
+	await I.wait(2);
+}); 
+
+
+Scenario("2. Read and report all the rows and columns of the table", async({I, Omayo}) =>
+{
+	await I.say('To read and report all the rows and columns of the table');
+	await omayo.table();
+	await I.wait(2);
+
+});
+
+Scenario("3. Login with username and password", async ({ I, Omayo }) => 
 {
   await I.say("When I enter username and password");
 	await omayo.value(username, email);
 	await omayo.value(paswd, pass);
 	await I.say("Then I click on login button");
-	await I.click('login');
-	await I.wait(20);
+	await I.click('Login');
+	await I.wait(10);
 
 });
+
+
+ 
+Scenario("4. To get text from preloaded text box", async({I, Omayo}) =>
+{
+	await I.say('Get text from the preloaded text box');
+	await omayo.text(preloadtext);
+	await I.wait(2);
+	
+}); 
+
+
+
+Scenario("5. Check the button if it is enabled/diabled", async({I, Omayo}) =>
+{
+	await I.say('When I click on button 2');
+	await omayo.btncheck();
+	await I.wait(2);
+});
+
+Scenario("6. To fill the search box with a text and check if the url has the  entered text", async({I, Omayo}) =>
+{
+	await I.say('When I enter a text in the search box');
+	await omayo.value(sbox, svalue);
+	await I.wait(2);
+	await I.say('when I click on search button');
+	await I.click('Search');
+	await I.wait(1);
+	await omayo.check();
+	await I.wait(2);
+
+
+});
+
+
+
+Scenario("7. Print all dropdown option in older newsletter and click doc1", async({I, Omayo}) =>
+{
+	await I.say('When I click on older newsletter');
+	await omayo.dropdisplay();
+	await I.say('click on doc1 in the dropdown');
+	await omayo.dselect();
+	await I.wait(2);
+});
+
+
+
+
+
+Scenario("8. To click on Delayed button to navigate to flipkart homepage", async({I, Omayo}) =>
+{
+	await I.say('When I click on delayed button');
+	await I.click('Dropdown');
+	await I.wait(2);
+	await omayo.selectflip();
+	await I.say('clicked on flipkart');
+	await omayo.validateflip();
+	await I.wait(2);
+	
+});
+
+
+
+Scenario("9. Validated error in iframe 2", async ({I, Omayo}) =>
+{
+	await I.say('Check error keyword in Iframe - 2');
+	await omayo.frame();
+	await I.wait(2);
+})
+
+
+
 
 
 
--- omayo.js
+++ omayo.js
@@ -2,7 +2,9 @@ const assert = require("assert");
 
 const I = actor();
 
-const fkart = "//span[text() = 'login']";
+const Flipkart = "a[text() = 'Flipkart' ] ";
+//const Button1 = 'button#but1';
+//const Button2 = 'button#but2';
 
 
 module.exports = {
@@ -14,71 +16,168 @@ module.exports = {
     I.waitForVisible("span#blogsmenu");
   },
 
-  enterTodo(todo) {
-    I.fillField(".new-todo", todo);
-    I.pressKey("Enter");
-  },
+  
 
   async value(loc,val)
   {
     I.fillField(loc,val);
   },
 
-  markAllAsCompleted() {
-    I.click('label[for="toggle-all"');
+  async hoverTo()
+  {
+    await I.moveCursorTo("span#blogsmenu");
   },
 
-  clearCompleted() {
-    I.click("button.clear-completed");
+
+  async select()
+  {
+    await I.click("//span[text()='SeleniumByArun']");
   },
 
-  filterAll() {
-    I.click(locate(".filters li").at(1));
+  async validate()
+  {
+    assert(await I.grabCurrentUrl, "https://selenium-by-arun.blogspot.com/","pass");
   },
 
-  filterActive() {
-    I.click(locate(".filters li").at(2));
+
+  async enable()
+  {
+    await I.seeElementEnabled('btn2');
   },
 
-  filterCompleted() {
-    I.click(locate(".filters li").at(3));
+  async disable()
+  {
+    await I.seeElementDisabled('btn1');
   },
 
-  editNthTodo(nthTodo, newTodoText) {
-    I.doubleClick(nthTodoItem(nthTodo));
-    I.fillField(nthTodoEditField(nthTodo), newTodoText);
-    I.pressKey("Enter");
+  async hoverToBtn1()
+  {
+    await I.moveCursorTo("button#but1");
   },
 
-  deleteNthTodo(nthTodo) {
-    // Use a custom helper function to hover over an todo item
-    I.moveCursorTo(`.todo-list li:nth-child(${nthTodo})`);
-    I.click(nthTTodoDeleteButton(nthTodo));
+  async hoverToBtn2()
+  {
+    await I.moveCursorTo("button#but2");
   },
 
-  refresh() {
-    I.refreshPage();
+
+  async selectBtn1()
+  {
+    await I.click("button#but1");
   },
 
-  async seeNthTodoEquals(nthTodo, todo) {
-    let todos = await I.grabTextFrom(".todo-list li");
-    if (typeof todos === "string") {
-      todos = [todos];
+  async validateBtn1()
+  {
+    await I.seeElement('button#but1'[this.disabled]);
+  },
+
+  async selectBtn2()
+  {
+    await I.click("button#but2");
+  },
+
+  async validateBtn2()
+  {
+    await I.seeElement('button#but2'[this.disabled]);
+  },
+
+
+  async text(loc)
+  {
+    let str = await I.grabAttributeFrom(loc, 'value');
+    console.log('Value retrieved from preloaded text box is: [${str}]');
+  },
+
+  async check()
+  {
+    let ctrUrl = I.grabCurrentUrl();
+    console.log('Current URL is [${ctrUrl}]');
+    let str = await I.grabAttributeFrom('input.gsc-input', 'value');
+    console.log('Entered text in the searchbox is [${str}]');
+    let result = ctrUrl.includes(str);
+    assert(result, "pass");
+    
+  },
+
+
+  async selectflip()
+  {
+    await I.click('Flipkart');
+  },
+
+  async validateflip()
+  {
+    assert(await I.grabCurrentUrl, "http://flipkart.com/","pass");
+  },
+
+  async dselect()
+  {
+    await I.selectOption("select#drop1", "doc 1");
+  },
+
+
+  async dropdisplay()
+  {
+    var opt = await I.grabTextFromAll('select#drop1');
+    let i;
+    console.log("Older newsletter dropdown options are:");
+    for (i=0; i<opt.length; i++)
+    {
+      let str = opt[i];
+      console.log(str);
     }
-
-    assert(todos[nthTodo - 1] === todo, `Expected "${todo}" but got "${todos[nthTodo - 1]}"`);
-    return todos;
   },
 
-  seeNumberOfTodos(numberOfTodos) {
-    I.seeNumberOfVisibleElements(".todo-list li", numberOfTodos);
+
+  async frame()
+  {
+    await I.scrollTo('text=Iframe1 & Iframe2');
+    await I.switchTo("//iframe[@id='iframe2']");
+    console.log('Iframe1 is switched to Iframe2');
+    await I.wait(2);
+    let i = I.grabTextFrom('#iframe2');
+    console.log('[${i}]');
+    assert("Error", i, "pass");
   },
 
-  seeEmptyTodoInput() {
-    I.seeInField(".new-todo", "");
+  async btncheck()
+  {
+    let x = I.grabTextFrom("//h2[text()='Enabled Button']");
+    console.log(x);
+    if((await x).includes("Enabled"))
+    {
+      console.log('Button 2 is enabled');
+    }
+    else
+    {
+      console.log('Button 2 is diabled');
+    }
+    await I.wait(2);
+
+    let y = I.grabTextFrom("//h2[text()='Disabled Button']");
+    console.log(y);
+    if((await y).includes("Disabled"))
+    {
+      console.log('Button 1 is disabled');
+    }
+    else
+    {
+      console.log('Button 1 is enabled');
+    }
   },
 
-  seeFooter() {
-    I.seeElement("footer.info");
+  async table()
+  {
+    await I.scrollTo('text=table');
+    let val = I.grabTextFromAll("(//div[@class='widget-content'])[5]//table/tr");
+    let i;
+    console.log("Table contents are: ");
+    for(i=0; i<val.length; i++)
+    {
+      let eachval = val[i];
+      console.log(eachval);
+    }
   },
+
+  
 };
