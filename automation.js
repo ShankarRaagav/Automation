eae145ba9efbf43ecdf402e9e0810fad556e271d
ShankarRaagav <127303292+ShankarRaagav@users.noreply.github.com>
2023-04-05 18:22:07 +0530

    Add files via upload

--- automation.js
+++ automation.js
@@ -1,6 +1,17 @@
 const { selectdrop } = require ("../Pages/omayo");
+const { event } = require('codeceptjs');
+const { config } = require('../../codecept.conf');
+
 const omayo = require("../Pages/omayo");
-const { seeNthTodoEquals } = require("../Pages/todoPages");
+const selenium = require("../Pages/Selenium by arun");
+const rowcol = require("../Pages/Table");
+const usrpsw = require("../Pages/Login");
+const preloadtxt = require("../Pages/Preloaded text");
+const enadis = require("../Pages/Enable disable");
+const scheck = require("../Pages/Search and check url");
+const doc1 = require("../Pages/Newsletter doc1");
+const flip = require("../Pages/Flipkart");
+const iframe = require("../Pages/Iframe2");
 
 Feature("Testing scenarios in omayo blogspot");
 
@@ -41,11 +52,11 @@ Scenario("Enter text in dialogue box", async({I, Omayo}) =>
 Scenario("1. Verify selenium by arun page after mouseover and click", async({I, Omayo})=>
 {
 	await I.say('Mouseover on blogsmenu and click on SeleniumbyArun');
-	await omayo.hoverTo();
+	await selenium.hoverTo();
 	await I.wait(2);
-	await omayo.select();
+	await selenium.select();
 	await I.say('SeleniumbyArun is shown in blogs dropdown');
-	await omayo.validate();
+	await selenium.validate();
 	await I.wait(2);
 }); 
 
@@ -53,7 +64,7 @@ Scenario("1. Verify selenium by arun page after mouseover and click", async({I, 
 Scenario("2. Read and report all the rows and columns of the table", async({I, Omayo}) =>
 {
 	await I.say('To read and report all the rows and columns of the table');
-	await omayo.table();
+	await rowcol.table();
 	await I.wait(2);
 
 });
@@ -61,8 +72,8 @@ Scenario("2. Read and report all the rows and columns of the table", async({I, O
 Scenario("3. Login with username and password", async ({ I, Omayo }) => 
 {
   await I.say("When I enter username and password");
-	await omayo.value(username, email);
-	await omayo.value(paswd, pass);
+	await usrpsw.value(username, email);
+	await usrpsw.value(paswd, pass);
 	await I.say("Then I click on login button");
 	await I.click('Login');
 	await I.wait(10);
@@ -74,7 +85,7 @@ Scenario("3. Login with username and password", async ({ I, Omayo }) =>
 Scenario("4. To get text from preloaded text box", async({I, Omayo}) =>
 {
 	await I.say('Get text from the preloaded text box');
-	await omayo.text(preloadtext);
+	await preloadtxt.text(preloadtext);
 	await I.wait(2);
 	
 }); 
@@ -84,19 +95,19 @@ Scenario("4. To get text from preloaded text box", async({I, Omayo}) =>
 Scenario("5. Check the button if it is enabled/diabled", async({I, Omayo}) =>
 {
 	await I.say('When I click on button 2');
-	await omayo.btncheck();
+	await enadis.btncheck();
 	await I.wait(2);
 });
 
-Scenario("6. To fill the search box with a text and check if the url has the  entered text", async({I, Omayo}) =>
+Scenario.only("6. To fill the search box with a text and check if the url has the  entered text", async({I, Omayo}) =>
 {
 	await I.say('When I enter a text in the search box');
-	await omayo.value(sbox, svalue);
+	await scheck.value(sbox, svalue);
 	await I.wait(2);
 	await I.say('when I click on search button');
-	await I.click('Search');
+	await I.click(Search);
 	await I.wait(1);
-	await omayo.check();
+	await scheck.check();
 	await I.wait(2);
 
 
@@ -107,9 +118,9 @@ Scenario("6. To fill the search box with a text and check if the url has the  en
 Scenario("7. Print all dropdown option in older newsletter and click doc1", async({I, Omayo}) =>
 {
 	await I.say('When I click on older newsletter');
-	await omayo.dropdisplay();
+	await doc1.dropdisplay();
 	await I.say('click on doc1 in the dropdown');
-	await omayo.dselect();
+	await doc1.dselect();
 	await I.wait(2);
 });
 
@@ -122,9 +133,9 @@ Scenario("8. To click on Delayed button to navigate to flipkart homepage", async
 	await I.say('When I click on delayed button');
 	await I.click('Dropdown');
 	await I.wait(2);
-	await omayo.selectflip();
+	await flip.selectflip();
 	await I.say('clicked on flipkart');
-	await omayo.validateflip();
+	await flip.validateflip();
 	await I.wait(2);
 	
 });
@@ -134,9 +145,9 @@ Scenario("8. To click on Delayed button to navigate to flipkart homepage", async
 Scenario("9. Validated error in iframe 2", async ({I, Omayo}) =>
 {
 	await I.say('Check error keyword in Iframe - 2');
-	await omayo.frame();
+	await iframe.frame();
 	await I.wait(2);
-})
+});
 
 
 
